bd1a83baf0b8ff7fea90be00798800cbc6ae35a1
danyal <danyallsty04@gmail.com>
2023-09-10 06:54:57 -0700

    slider component created

---
+++ src/components/Slider/HeaderSlider.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import "./HeaderSlider.scss";
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import sliderImgs from '../../assets/images/slider_img_1.jpg'
+import sliderImgs2 from '../../assets/images/slider_img_2.jpg'
+
+const HeaderSlider = () => {
+  let settings = {
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+
+  return (
+    <div className='slider'>
+      <div className='container'>
+        <div className='slider-content overflow-x-hidden'>
+          <Slider {...settings}>
+            <div className='slider-item'>
+              <img src = {sliderImgs} alt = "" />
+            </div>
+            <div className='slider-item'>
+              <img src = {sliderImgs2} alt = "" />
+            </div>
+          </Slider>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HeaderSlider
