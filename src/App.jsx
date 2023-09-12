26ae9e8705e7e48357e12efbba458db79e4ad543
danyal <danyallsty04@gmail.com>
2023-09-12 07:54:48 -0700

    details

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
 import './App.scss'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { Home } from './pages'
+import { CartPage, CategoryProduct, Home, ProductSinglePage } from './pages'
 import Header from './components/Header/Header'
+import Sidebar from './components/Sidebar/Sidebar'
+import Footer from './components/Footer/Footer'
+import Signup from './pages/Signup/Signup'
 
 function App() {
 
@@ -11,9 +14,16 @@ function App() {
 
     <Router>
        <Header/>
+       <Sidebar/>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/product/:id" element={<ProductSinglePage/>} />
+        <Route path="/category/:category" element={<CategoryProduct/>} />
+        <Route path="/cart" element={<CartPage/>} />
+        <Route path="/signup" element={<Signup/>} />
+      
       </Routes>
+      <Footer/>
     </Router>
       
     </>
