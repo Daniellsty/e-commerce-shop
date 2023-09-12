ee41b650126a4e6d842b0416ad7545e9956aa4a0
danyal <danyallsty04@gmail.com>
2023-09-12 06:48:43 -0700

    category product component created

--- src/pages/CategoryProduct/CategoryProduct.jsx
+++ src/pages/CategoryProduct/CategoryProduct.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, {useEffect} from 'react';
+import "./CategoryProductPage.scss";
+import ProductList from "../../components/ProductList/ProductList";
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { getAllProductsByCategory, fetchAsyncProductsOfCategory, getCategoryProductsStatus } from '../../redux/categorySlice';
+import Loader from '../../components/Loader/Loader';
+import { STATUS } from '../../utils/status';
 
-const CategoryProduct = () => {
+const CategoryProductPage = () => {
+  const dispatch = useDispatch();
+  const { category } = useParams();
+  const categoryProducts = useSelector(getAllProductsByCategory);
+  
+  useEffect(() => {
+    dispatch(fetchAsyncProductsOfCategory(category));
+  }, [ category]);
+
+
+  
+  const categoryProductsStatus = useSelector(getCategoryProductsStatus);
   return (
-    <div>CategoryProduct</div>
+    <div className='cat-products py-5 bg-whitesmoke'>
+      <div className='container'>
+        <div className='cat-products-content'>
+          <div className='title-md'>
+            <h3>See our <span className='text-capitalize'>{category.replace("-", " ")}</span></h3>
+          </div>
+
+          {
+            categoryProductsStatus === STATUS.LOADING ? <Loader /> : <ProductList products = {categoryProducts} />
+          }
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default CategoryProduct
+export default CategoryProductPage
